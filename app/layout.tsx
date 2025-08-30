277efe40ddbbd16bfbfd6a072c7cdd61abf2b3c7
Srihar1-raman <srihari22work@gmail.com>
2025-08-30 19:10:07 +0530

    Fix font loading for consistent Comic Sans MS display across all devices (iPhone, Android, Desktop)

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preload" href="https://fonts.gstatic.com/s/comicsansms/v1/4UaDrEJFqYhaShnGvWF9Bg.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="https://fonts.gstatic.com/s/comicsansms/v1/4UaDrEJFqYhaShnGvWF9Bg.woff" as="font" type="font/woff" crossOrigin="anonymous" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
